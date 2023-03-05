05a73578dc04fd82c958c6e0e0f38e82c6b513e0
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-05 18:36:12 -0500

    Create a instance of menu class

--- labNetPractica1/labNetPractica1/Program.cs
+++ labNetPractica1/labNetPractica1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Poo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace labNetPractica1
     {
         static void Main(string[] args)
         {
+            Menu menu = new Menu();
+            menu.MostrarEncabezado();
         }
     }
 }
