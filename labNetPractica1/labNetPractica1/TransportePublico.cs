15c3df42a789adbc8c2d958dea7c0e31aa0965e1
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-05 18:32:28 -0500

    Add TransportePublico class

---
+++ labNetPractica1/labNetPractica1/TransportePublico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Poo
+{
+    public abstract class TransportePublico
+    {
+        private int _pasajeros { get; set; }
+
+        public TransportePublico(int _pasajeros)
+        {
+            this._pasajeros = _pasajeros;
+        }
+
+        public int get_pasajeros()
+        {
+            return _pasajeros;
+        }
+
+        public abstract void Avanzar();
+
+        public abstract void Detenerse();
+
+        public abstract void MostrarPasajeros();
+    }
+}
