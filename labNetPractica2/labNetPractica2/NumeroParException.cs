771c6ceb1dc5f6c510b22422b75fbf60842a1ae8
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-07 21:48:13 -0500

    Add new tipe of exception

---
+++ labNetPractica2/labNetPractica2/NumeroParException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labNetPractica2
+{
+    public class NumeroParException : Exception
+    {
+        public NumeroParException()
+            : base("Ops! no se permiten numeros impares") { }
+    }
+}
