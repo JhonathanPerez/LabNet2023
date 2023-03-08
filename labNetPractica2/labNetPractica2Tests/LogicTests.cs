c6523b7cba1fa2d0d53662c2b16b4580c26eaab6
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-07 21:17:12 -0500

    Add test cases

---
+++ labNetPractica2/labNetPractica2Tests/LogicTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using labNetPractica2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labNetPractica2.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArrayTypeMismatchException))]
+        public void DispararExecpcionTest()
+        {
+            Logic logic = new Logic();
+            logic.DispararExecpcion();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(LetraException))]
+        public void DispararExecpcionPersonalizadaTest()
+        {
+            Logic logic = new Logic();
+            logic.DispararExecpcionPersonalizada();
+        }
+    }
+}
---
+++ labNetPractica2/labNetPractica2Tests/OperacionesTests.cs
@@ -0,0 +1,35 @@
+﻿using labNetPractica2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace labNetPractica2.Tests
+{
+    [TestClass()]
+    public class OperacionesTests
+    {
+        [TestMethod()]
+        public void DividirTest()
+        {
+            int numero1 = 10;
+            int numero2 = 2;
+            int resultadoEsperado = 5;
+            int resultadoActual;
+
+            Operaciones operacion = new Operaciones();
+            resultadoActual = operacion.Dividir(numero1, numero2);
+
+            Assert.AreEqual(resultadoActual, resultadoEsperado);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(System.DivideByZeroException))]
+        public void DividirTest1()
+        {
+            int numero1 = 10;
+            int numero2 = 0;
+
+            Operaciones operacion = new Operaciones();
+            operacion.Dividir(numero1, numero2);
+        }
+    }
+}
