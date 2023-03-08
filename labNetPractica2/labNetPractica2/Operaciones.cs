b5846b2bf979966960702c1b755337df6f523719
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-07 21:15:48 -0500

    Add operaciones class

---
+++ labNetPractica2/labNetPractica2/Operaciones.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace labNetPractica2
+{
+    public class Operaciones
+    {
+        public int Dividir(int numero1, int numero2)
+        {
+            try
+            {
+                return numero1 / numero2;
+            }
+            catch (DivideByZeroException)
+            {
+                throw;
+            }
+            catch (FormatException)
+            {
+                throw;
+            }
+        }
+    }
+}
