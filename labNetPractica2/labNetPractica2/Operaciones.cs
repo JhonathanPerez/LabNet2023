e569d41c97f2cdb681c677b12caebfc7bcc095ba
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-07 21:34:57 -0500

    Delete try catch

--- labNetPractica2/labNetPractica2/Operaciones.cs
+++ labNetPractica2/labNetPractica2/Operaciones.cs
@@ -6,18 +6,7 @@ namespace labNetPractica2
     {
         public int Dividir(int numero1, int numero2)
         {
-            try
-            {
-                return numero1 / numero2;
-            }
-            catch (DivideByZeroException)
-            {
-                throw;
-            }
-            catch (FormatException)
-            {
-                throw;
-            }
+            return numero1 / numero2;
         }
     }
 }
