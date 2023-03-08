1470116a88b26e9e1bd1332a3a0c1f2b153c9e36
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-07 21:16:25 -0500

    Add logic class

---
+++ labNetPractica2/labNetPractica2/Logic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labNetPractica2
+{
+    public class Logic
+    {
+        public void DispararExecpcion()
+        {
+            throw new ArrayTypeMismatchException();
+        }
+
+        public void DispararExecpcionPersonalizada()
+        {
+            throw new LetraException();
+        }
+    }
+}
