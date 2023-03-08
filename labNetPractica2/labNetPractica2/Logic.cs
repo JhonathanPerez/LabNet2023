0ef047c480419bb7d8a4ba23e753f60443c671d2
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-08 14:06:28 -0500

    Fix name of method

--- labNetPractica2/labNetPractica2/Logic.cs
+++ labNetPractica2/labNetPractica2/Logic.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace labNetPractica2
 {
     public class Logic
     {
-        public void DispararExecpcion()
+        public void DispararExcepcion()
         {
             throw new ArrayTypeMismatchException();
         }
