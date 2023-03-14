d0db0ff45a1521bf51e18f7b46d9186259f60218
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-14 17:04:40 -0500

    Add BaseLogic class

---
+++ Lab.EF/Lab.EF.Logic/BaseLogic.cs
@@ -0,0 +1,18 @@
+﻿using Lab.EF.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.EF.Logic
+{
+    public class BaseLogic
+    {
+        protected NorthwindContext _nortwindContext;
+
+        public BaseLogic() { 
+            _nortwindContext = new NorthwindContext();
+        }
+    }
+}
