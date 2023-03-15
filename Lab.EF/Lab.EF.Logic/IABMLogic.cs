279ae20b2b7931f6bba33dbb4ba46cb018a6b027
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-14 21:26:52 -0500

    Add interface

---
+++ Lab.EF/Lab.EF.Logic/IABMLogic.cs
@@ -0,0 +1,27 @@
+﻿using Lab.EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.EF.Logic
+{
+    public interface IABMLogic<T>
+    {
+        List<T> GetAll();
+
+        void Add(T type);
+
+        T ItemExist(int id);
+
+        bool Delete(int id);
+
+        void Update(T newCategorie);
+
+
+       
+
+
+    }
+}
