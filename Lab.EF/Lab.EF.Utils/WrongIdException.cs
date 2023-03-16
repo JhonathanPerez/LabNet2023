fcbbb57c9356b6e81cb608d5d21453a1c5260d0d
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-15 19:32:44 -0500

    Add utils layer

---
+++ Lab.EF/Lab.EF.Utils/WrongIdException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.EF.Utils
+{
+    public class WrongIdException : Exception
+    {
+        public WrongIdException():base("El id ingresado no existe en la base de datos") { }
+    }
+}
