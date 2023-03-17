b1e2dbc642181504fab452262143bbccf4a7c70e
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-17 14:26:00 -0500

    Add OrdersLogic

---
+++ Lab.EF/Lab.EF.Logic/OrdersLogic.cs
@@ -0,0 +1,18 @@
+﻿using Lab.EF.Data;
+using Lab.EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.EF.Logic
+{
+    public class OrdersLogic : BaseLogic, IABMLogic<Orders>
+    {
+        public List<Orders> GetAll()
+        {
+            return _nortwindContext.Orders.ToList();
+        }
+    }
+}
