08127f6aa7407b49115ecb3db0b74ee154259349
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-17 14:26:20 -0500

    Add CustomerLogic

---
+++ Lab.EF/Lab.EF.Logic/CustomersLogic.cs
@@ -0,0 +1,17 @@
+﻿using Lab.EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.EF.Logic
+{
+    public class CustomersLogic : BaseLogic, IABMLogic<Customers>
+    {
+        public List<Customers> GetAll()
+        {
+            return _nortwindContext.Customers.ToList();
+        }
+    }
+}
