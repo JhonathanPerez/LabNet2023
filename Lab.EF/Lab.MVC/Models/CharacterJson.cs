0f7328427443afafdad37cba8cd109ae9bfd4927
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2023-03-26 17:12:54 -0500

    Add character model

---
+++ Lab.EF/Lab.MVC/Models/CharacterJson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab.MVC.Models
+{
+    public class CharacterJson
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string species { get; set; }
+        public string gender { get; set; }
+        public string image { get; set; }
+    }
+}
